7872b81f040ec58d0684e414bc6d28cf7470c87b
Cynthia Castillo Millán <lobo4wolf4@hotmail.com>
2018-05-07 21:19:42 -0500

    QR reader added

---
+++ app/src/main/java/com/restaurant/app/activities/QR_reader.java
@@ -0,0 +1,13 @@
+package com.restaurant.app.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QR_reader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_reader);
+    }
+}
---
+++ app/src/main/java/com/restaurant/app/models/Login.java
@@ -0,0 +1,8 @@
+package com.restaurant.app.models;
+
+/**
+ * Created by Liz_Wolf on 07/05/2018.
+ */
+
+public class Login {
+}
